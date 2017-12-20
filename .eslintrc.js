fd8b8d648df9f08e9fca72b8a7e4508132b5aeeb
Andrew Vorakrajangthiti <andrewjvora@gmail.com>
2017-12-20 00:33:39 -0800

    Added and configured eslint using standard model.
    Use "npm run lint" to run lint on the project.
    
    I recommend having a custom start command that runs lint and then launches
    the app.

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "mocha": true
+    },
+    "extends": "standard",
+    "rules": {
+        "indent": ["error", 4],
+        "space-before-function-paren": ["error", "never"],
+        "camelcase": 0
+    }
+};
